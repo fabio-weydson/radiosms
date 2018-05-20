353899a6784f388482e2db35084bfe42d0c29860
fabio-weydson <fabioweydson@gmail.com>
2018-05-20 14:08:03 -0300

    testSMS4

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -106,7 +106,7 @@ angular.module('starter.controllers', [])
   //$scope.$on('$ionicView.enter', function(e) {
   //});
 
-  $scope.chats = Chats.all();
+ // $scope.chats = Chats.all();
   $scope.remove = function(chat) {
     Chats.remove(chat);
   };
--- www/js/services.js
+++ www/js/services.js
@@ -24,49 +24,48 @@ angular.module('starter.services', [])
     }
   };
 })
-.factory('Chats', function($scope) {
-  $scope.chats = [];
+.factory('Chats', function() {
+  var chats = [];
   // Might use a resource here that returns a JSON array
   if(SMS) SMS.listSMS({}, function(data){
     alert('sms listed as json array');
     //updateData( JSON.stringify(data) );
-    
+    var smsList = [];
       if(Array.isArray(data)) {
         for(var i in data) {
           var sms = data[i];
-          smsList.push(sms);
-          $scope.chats += sms.address + ": " + sms.body + "<br/>";
+          chats.push(sms.address);
         }
       }      
     }, function(err){
      alert('error list sms: ' + err);
     });
   // Some fake testing data
-  var chats = [{
-    id: 0,
-    name: '+55 95484 8464',
-    lastText: 'Lorem ipsum dolor sit amet... ',
-  }, {
-    id: 1,
-    name: '+55 95484 4884',
-    lastText: 'Lorem ipsum dolor sit amet...',
-  }, {
-    id: 2,
-    name: '+55 95484 4844',
-    lastText: 'Lorem ipsum dolor sit amet...',
-  }, {
-    id: 3,
-    name: '+55 95484 4884',
-    lastText: 'Lorem ipsum dolor sit amet...',
-  }, {
-    id: 4,
-    name: '+55 98484 4846',
-    lastText: 'Lorem ipsum dolor sit amet...',
-  }];
+  // var chats = [{
+  //   id: 0,
+  //   name: '+55 95484 8464',
+  //   lastText: 'Lorem ipsum dolor sit amet... ',
+  // }, {
+  //   id: 1,
+  //   name: '+55 95484 4884',
+  //   lastText: 'Lorem ipsum dolor sit amet...',
+  // }, {
+  //   id: 2,
+  //   name: '+55 95484 4844',
+  //   lastText: 'Lorem ipsum dolor sit amet...',
+  // }, {
+  //   id: 3,
+  //   name: '+55 95484 4884',
+  //   lastText: 'Lorem ipsum dolor sit amet...',
+  // }, {
+  //   id: 4,
+  //   name: '+55 98484 4846',
+  //   lastText: 'Lorem ipsum dolor sit amet...',
+  // }];
 
   return {
     all: function() {
-      return $scope.chats;
+      return chats;
     },
     remove: function(chat) {
       chats.splice(chats.indexOf(chat), 1);
